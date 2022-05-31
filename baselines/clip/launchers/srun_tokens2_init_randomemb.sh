b6f626676db45eceb3db3eb376f608141b10fcad
robertodessi <roberto.dessi11@gmail.com>
2022-05-31 04:26:38 -0700

    change param in srun script

--- baselines/clip/launchers/srun_tokens2_init_randomemb.sh
+++ baselines/clip/launchers/srun_tokens2_init_randomemb.sh
@@ -4,7 +4,7 @@ date
 echo "BEGIN"
 
 cd ../
-python /private/home/rdessi/imagecode/baselines/clip/contra_soft_prompt.py --n_tokens 2 --prompt_init "random_embedding" --pretraining_epochs 5
+python /private/home/rdessi/imagecode/baselines/clip/contra_soft_prompt.py --n_tokens 2 --prompt_init "random_embedding"
 
 date
 echo "ALL DONE"
